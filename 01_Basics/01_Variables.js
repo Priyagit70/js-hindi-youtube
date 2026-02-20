2cfec6ebf876b20bfe468f0d230a9a4dcc1ecfc5
Priya Rawat <102343698+Priyagit70@users.noreply.github.com>
2026-02-20 07:23:25 +0000

    variables done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 155432
+let email = "priya@gmail.com"
+var accPassword = "1234"
+acountCity = "Kotdwar"
+
+let accountState;
+
+
+email = "pr@pr.com"
+accPassword = "1345"
+acountCity = "dehradun"
+
+
+console.table([accountId, email, accPassword, acountCity, accountState])
