00d03a5cdf5481469b940a3557264bb32c8e50ae
Priya Rawat <102343698+Priyagit70@users.noreply.github.com>
2026-02-23 08:52:29 +0000

    datatypes done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all script in newer version/js
+
+// alert (3 +3) // we sre using nodejs, not browser.
+
+
+
+// number => 2 to power 52/53
+// bigint
+// boolean => true/false
+//string => ""
+//undefined
+//null => standalone value
+// symbol => unique
+
+//object
+
+console.log(typeof null);  // output: object
+console.log(typeof undefined);  // output: undefined
