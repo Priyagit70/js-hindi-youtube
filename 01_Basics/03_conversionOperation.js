0277d8574abb26544f2e1d04a262aa6c3ce15162
Priya Rawat <102343698+Priyagit70@users.noreply.github.com>
2026-02-25 05:57:32 +0000

    conversion done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = null
+
+// console.log(score);
+// console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber); 
+
+// "33" => number
+// "33abc" => NaN
+// "true" => 1, false => 0
+
+let isLoggedIn = NaN
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false 
+// "" => false; "priya" => true 
+// undefined => false; null=> false; NaN => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
